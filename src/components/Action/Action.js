1b9a0ff1cec9eb815abda0911008d8d0dab8234a
Nikita <nikitadv777@gmail.com>
2020-07-10 16:14:50 +0300

    Pulsating effect

--- src/components/Action/Action.js
+++ src/components/Action/Action.js
@@ -3,15 +3,6 @@ import { Preloader } from '../Preloader';
 import styles from './Action.module.scss';
 
 export const Action = ({ chosenAction, isBoy, isLoading }) => {
-  // const [isLoading, setIsLoading] = useState(true);
-
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   const timer = setTimeout(() => {
-  //     setIsLoading(false);
-  //   }, 2000);
-  //   return () => clearTimeout(timer);
-  // }, [chosenAction]);
   if (isLoading) {
     return (
       <div className={styles['action-place-block']}>
