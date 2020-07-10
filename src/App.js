8229f37ab785886ec37f73239807cbab9d493b65
Nikita <nikitadv777@gmail.com>
2020-07-10 23:17:05 +0300

    Removed loader

--- src/App.js
+++ src/App.js
@@ -5,31 +5,11 @@ import Fants from './pages/Fants/Fants';
 import Layout from './components/Layout/Layout';
 import Menu from './pages/Menu/Menu';
 import Poses from './pages/Poses/Poses';
-import Loader from './components/Loader/Loader';
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isRemoved, setIsRemoved] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-    return () => clearTimeout(timer);
-  }, []);
-  useEffect(() => {
-    setIsRemoved(false);
-    const timer = setTimeout(() => {
-      setIsRemoved(true);
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <>
       <BrowserRouter>
-        {!isRemoved && <Loader isLoading={isLoading} />}
         <Layout>
           <Route exact path="/" component={Menu} />
           <Route exact path="/fants" component={Fants} />
