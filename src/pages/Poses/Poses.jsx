52fb070f1beaf067ce91c069d20bffd180655bce
Nikita <nikitadv777@gmail.com>
2020-07-10 22:57:25 +0300

    Default image for pose page

--- src/pages/Poses/Poses.jsx
+++ src/pages/Poses/Poses.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Poses.module.scss';
+import { poses, defaultPose } from '../../state/poses';
 
 const Poses = () => {
+  const [chosenPose, setChosenPose] = useState(defaultPose);
+
+  const randomizePose = () => {};
+
   return (
     <div>
       <h1 className={styles.header}>Выбери лучшую позу</h1>
+      <div className={styles.poseContainer}>
+        <img src={chosenPose.src} alt="pose" />
+      </div>
+      <div className={styles.buttonContainer}>
+        <img
+          src="https://res.cloudinary.com/dygwe02oe/image/upload/v1594409197/N30QdlVHvViVE3z2xakUpNN4pAXi7kef8XXBy_gbBB4awzet5xy02jC_bqM2m3_jrlY_vykl8j.png"
+          alt="start"
+        />
+      </div>
     </div>
   );
 };
---
+++ src/state/poses.js
@@ -0,0 +1,86 @@
+export const defaultPose = {
+  src:
+    'https://res.cloudinary.com/dygwe02oe/image/upload/v1594410485/IMG_5267_lc6p4q.jpg',
+};
+export const poses = [
+  {
+    id: 1,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594323769/nozhnicy-i-doggi_1484848074_j3d9cz.jpg',
+  },
+  {
+    id: 2,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594323812/doggi-stoya_1484847695_sdkdvt.jpg',
+  },
+  {
+    id: 3,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594323843/nogi-vverx_1484847520-e1485257377418-630x450_d9fnpp.jpg',
+  },
+  {
+    id: 4,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594323965/irish-garden-sex-position2_1487783647-630x350_bezrzy.jpg',
+  },
+  {
+    id: 5,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594324203/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDEzLzEyL2Jyb2R1ZGUucnVfMTMuMTIuMjAxM191Wlk5N0thaGVsMG4wLmpwZw_xwqaty.jpg',
+  },
+  {
+    id: 6,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594324255/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDEzLzEyL2Jyb2R1ZGUucnVfMTMuMTIuMjAxM19JMjZBdmNGVEw3b3E2LmpwZw_ibjncc.jpg',
+  },
+  {
+    id: 7,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594324269/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDEzLzEyL2Jyb2R1ZGUucnVfMTMuMTIuMjAxM184SnZhdjMwMVJnM1pmLmpwZw_mubuma.jpg',
+  },
+  {
+    id: 8,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594324282/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDEzLzEyL2Jyb2R1ZGUucnVfMTMuMTIuMjAxM190cExleFVqNnBGNDlkLmpwZw_y8tbdw.jpg',
+  },
+  {
+    id: 9,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594324301/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDEzLzEyL2Jyb2R1ZGUucnVfMTMuMTIuMjAxM183VjdWcGtQNTRmZzZ6LmpwZw_k8whza.jpg',
+  },
+  {
+    id: 10,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594324328/aHR0cHM6Ly9icm9kdWRlLnJ1L3dwLWNvbnRlbnQvdXBsb2Fkcy8yMDEzLzEyL2Jyb2R1ZGUucnVfMTMuMTIuMjAxM19iVlJmajZwMHprTk1rLmpwZw_xggtyn.jpg',
+  },
+  {
+    id: 11,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594324393/wheel-barrow-standing_1_e8s8g7.jpg',
+  },
+  {
+    id: 12,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594324826/kama-sutra-sex-pose-man-600w-1015840834_w5solp.jpg',
+  },
+  {
+    id: 13,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594325085/kama-sutra-sexual-pose-sex-600w-1015836502_gxawmg.jpg',
+  },
+  {
+    id: 14,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594325127/kama-sutra-sexual-pose-sex-600w-1015836487_cwn2dm.jpg',
+  },
+  {
+    id: 15,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594325197/kama-sutra-sex-pose-man-600w-1015840816_vid7ht.jpg',
+  },
+  {
+    id: 16,
+    src:
+      'https://res.cloudinary.com/dygwe02oe/image/upload/v1594325235/kama-sutra-sex-pose-man-600w-1015840804_uc0omj.jpg',
+  },
+];
