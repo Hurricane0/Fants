aaca4d0c8fe898f725b4c9bdbb1150feb85b98bf
web.oleg.fedorov <web.oleg.fedorov@gmail.com>
2021-10-27 09:45:27 +0300

    Netlify test

--- src/state/menu.js
+++ src/state/menu.js
@@ -1,7 +1,7 @@
 export const modes = [
   {
     id: 1,
-    title: 'Фанты',
+    title: 'ТЕСТ',
     src:
       'https://res.cloudinary.com/dygwe02oe/image/upload/v1594319964/dice_fjjm3r.png',
     link: '/fants',
